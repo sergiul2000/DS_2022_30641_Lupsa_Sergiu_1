e31c92b6cf05db99c3ed898bf8711229cd6b51bd
Lupsa Sergiu <sergiul2000@yahoo.com>
2022-11-09 17:20:51 +0200

    initial commit

---
+++ src/main/java/com/example/DS_2022_30641_Lupsa_Sergiu_1_Backend/Ds202230641LupsaSergiu1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.DS_2022_30641_Lupsa_Sergiu_1_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds202230641LupsaSergiu1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ds202230641LupsaSergiu1BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DS_2022_30641_Lupsa_Sergiu_1_Backend/Ds202230641LupsaSergiu1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DS_2022_30641_Lupsa_Sergiu_1_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ds202230641LupsaSergiu1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
